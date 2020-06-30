b2d3b7e4433b677b64e62c51eebba3dc0ee2b462
softbankun <ban@slco.jp>
2020-06-30 19:44:34 +0900

    必要素材配置
    使いそうなオブジェクトをasset storeよりDLしてインポート＆配置した

---
+++ Assets/CameraWork.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour {
+    Camera cam;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        cam.gameObject.transform.Translate(new Vector3(0.0f, 0.0f, 9.0f));
+    }
+}
---
+++ Assets/StartController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+	//　スタートボタンを押したら実行する
+	public void StartStore()
+	{
+		SceneManager.LoadScene("ECstore");
+	}
+}
